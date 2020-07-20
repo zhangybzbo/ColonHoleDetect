ae8151566795580250ca20a58265d63cc691cd40
zhangybzbo <42792266+zhangybzbo@users.noreply.github.com>
2020-07-20 15:32:34 -0400

    centerline

--- HoleDetect.py
+++ HoleDetect.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 import vtk
 import math
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ from skimage import morphology
 import skimage.feature
 from scipy import ndimage
 
-ColorBackground = [0.0, 0., 0.]
+ColorBackground = [0, 0, 0]
 
 def visual_pc(pc_poly, name):
     vertexFilter = vtk.vtkVertexGlyphFilter()
@@ -127,11 +128,41 @@ class colon(object):
     def set_centerline(self, resolution):
         xmin,xmax, ymin,ymax, zmin,zmax = self.obj.GetBounds()
         center = self.obj.GetCenter() # colon center
+        step = [zmin, (zmin+center[2])/2, center[2], (zmax+center[2])/2, zmax]
+        posx, posy, posx2, posy2 = [], [], [], []
+        negx, negy, negx2, negy2 = [], [], [], []
+        N = self.obj.GetNumberOfPoints()
+        for i in range(N):
+            p = [0] * 3
+            self.obj.GetPoint(i, p)
+            if p[2]>step[3]:
+                posx.append(p[0])
+                posy.append(p[1])
+            elif p[2]>step[2]:
+                posx2.append(p[0])
+                posy2.append(p[1])
+            elif p[2]>step[1]:
+                negx2.append(p[0])
+                negy2.append(p[1])
+            else:
+                negx.append(p[0])
+                negy.append(p[1])
         # key points
+        c_posx, c_posy = np.average(np.array(posx)), np.average(np.array(posy))
+        c_posx2, c_posy2 = np.average(np.array(posx2)), np.average(np.array(posy2))
+        c_negx, c_negy = np.average(np.array(negx)), np.average(np.array(negy))
+        c_negx2, c_negy2 = np.average(np.array(negx2)), np.average(np.array(negy2))
         pts = vtk.vtkPoints()
-        pts.InsertNextPoint([center[0], center[1], zmin])
-        pts.InsertNextPoint(center)
-        pts.InsertNextPoint([center[0], center[1], zmax])
+        # pts.InsertNextPoint([c_posx, c_posy, zmax])
+        pts.InsertNextPoint([np.average(np.array(posx)), np.average(np.array(posy)), step[4]])
+        pts.InsertNextPoint([np.average(np.array(posx2)), np.average(np.array(posy2)), step[3]])
+        #pts.InsertNextPoint([center[0], center[1], zmin])
+        #pts.InsertNextPoint([(c_negx2+c_posx2)/2, (c_negy2+c_posy2)/2, 0])
+        pts.InsertNextPoint([np.average(np.array(negx2+posx2)), np.average(np.array(negy2+posy2)), step[2]])
+        pts.InsertNextPoint([np.average(np.array(negx2)), np.average(np.array(negy2)), step[1]])
+        pts.InsertNextPoint([np.average(np.array(negx)), np.average(np.array(negy)), step[0]])
+        # pts.InsertNextPoint([c_negx, c_negy, zmin])
+        #pts.InsertNextPoint([center[0], center[1], zmax])
         
         # spline from keypoints
         spline = vtk.vtkParametricSpline() 
@@ -191,7 +222,7 @@ class colon(object):
             newcut.DeepCopy(cutline)
             self.all_cs[i] = newcut
             self.add_render(newcut, 'cut%d' % i, (255, 0, 0))
-                        
+
             n_p = newcut.GetNumberOfPoints()
             
             # origin direction on plane, [0, tz, -ty]
@@ -230,7 +261,7 @@ class colon(object):
                 for j in range(n_p):
                     self.arrays[0].append(i*self.cl_scale)
                     self.arrays[1].append(ave_dis*theta_ls[j])
-                    self.cs_points.InsertNextPoint(i*self.cl_scale, ave_dis*theta_ls[j], 0)   
+                    self.cs_points.InsertNextPoint(i*self.cl_scale, ave_dis*theta_ls[j], 0)
         
         # ave dis
         if scale == 'global':
@@ -261,35 +292,58 @@ def np_img(obj, scale=255):
     
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train RNN depth")
-    parser.add_argument("--chuck", type=str)
+    import os
+    from os import listdir
+    from os.path import isfile, join
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--chuck_dir", type=str, default='../mesh/')
+    parser.add_argument("--save_dir", type=str, default='dump/')
     parser.add_argument("--r_version", choices=['cs', 'global'])
     parser.add_argument("--closing", action="store_true")
     parser.add_argument("--opening", action="store_true")
     parser.add_argument("--disc_size", type=int, default=2)
     args = parser.parse_args()
     print(args)
-    
-    save_root = args.chuck + '_' + args.r_version
-    objects = colon(args.chuck + '.obj')
-    objects.set_centerline(200)
-    objects.add_render(objects.centerline, 'centerline', (255, 255, 0))
-    objects.cs_cut(args.r_version)
-    objects.visual()
-    visual_pc(objects.cs_deform, save_root + '.png')
-    
-    img, mapping = np_img(objects, scale=1)
-    plt.imsave(save_root+'_2d.png', img)
-    save_mor = save_root
-    if args.closing:
-        img = morphology.binary_closing(img, morphology.diamond(args.disc_size)).astype(np.uint8)
-        save_mor += '_close'
-    if args.opening:
-        img = morphology.binary_opening(img, morphology.diamond(args.disc_size)).astype(np.uint8)
-        save_mor += '_open'
-    plt.imsave(save_mor+'.png', img)
-    labeled, nr_objects = ndimage.label(img < 1) 
-    print("Number of holes is {}".format(nr_objects))
-    
-    edges = skimage.feature.canny(image=img*255)
-    plt.imshow(edges)
+
+
+    chucks = [f for f in listdir(args.chuck_dir) if isfile(join(args.chuck_dir, f))]
+    for chuck in chucks[::-1]:
+        save_root = args.save_dir + os.path.splitext(chuck)[0] + '_' + args.r_version
+        start = time.time()
+        objects = colon(join(args.chuck_dir, chuck))
+        print(time.time() - start)
+        objects.set_centerline(200)
+        print(time.time() - start)
+        objects.add_render(objects.centerline, 'centerline', (255, 255, 0))
+        objects.cs_cut(args.r_version)
+        print(time.time() - start)
+        objects.visual()
+        visual_pc(objects.cs_deform, save_root + '.png')
+
+        start = time.time()
+        img, mapping = np_img(objects, scale=1)
+        plt.imsave(save_root+'_2d.png', img*255, cmap='gray')
+        save_mor = save_root
+        if args.closing:
+            img = morphology.binary_closing(img, morphology.diamond(args.disc_size)).astype(np.uint8)
+            save_mor += '_close'
+        if args.opening:
+            img = morphology.binary_opening(img, morphology.diamond(args.disc_size)).astype(np.uint8)
+            save_mor += '_open'
+
+        img = img.astype(float)
+        labeled, nr_objects = ndimage.label(img < 1.0)
+        save_img = np.repeat(np.expand_dims(img, 2), 3, axis=2)
+        areas = ''
+        for i in range(nr_objects):
+            area = np.count_nonzero(labeled == i+1)
+            areas += '%d ' % area
+            save_img[:,:,0][labeled == i+1] = i / nr_objects
+        plt.imsave(save_mor+'.png', save_img)
+        print("%s: Number of holes is %d, area %s" % (chuck, nr_objects, areas))
+        print(time.time()-start)
+        input()
+
+        # edges = skimage.feature.canny(image=img*255)
+        # plt.imshow(edges)s
